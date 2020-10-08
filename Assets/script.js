6e3bfa79b4e0357171a6f1ab8b55e940aef693f3
Sam Fan <samisfan@msn.com>
2020-10-07 18:52:01 -0600

    progress in js, defining arrays

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,14 @@ var pwLength;
 var lower;
 var upper;
 var num;
-var specChar = ["Donut's"];
+var specChar;
+
+var characters = {
+  lower: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,",
+  upper: "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
+  num: "0,1,2,3,4,5,6,7,8,9",
+  specChar: "",
+};
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
