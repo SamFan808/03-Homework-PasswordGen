e5e8cbb7160da44bee150e43ff9b350777b7be1d
Sam Fan <samisfan@msn.com>
2020-10-10 17:55:43 -0600

    random character gen functioning! mostly, still need to populate to password field

--- Assets/script-201010.js
+++ Assets/script-201010.js
@@ -1,16 +1,10 @@
 // Defining variables
+var writePassword;
+var passwordText;
+
 
 // variable userInput stores user choice for password length into global memory
-// Write password to the #password input  
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function pwLength() {
   var userInput = prompt("How long would you like your password to be? Choose between 8 characters min and 128 characters max");
 
   // alerts user to choose a password length between 8 and 128
@@ -19,18 +13,48 @@ function writePassword() {
   
     if (userInput < 8 || userInput > 128) {
     alert("Please choose a number between 8 and 128");
-    return writePassword();
+    return pwLength();
 
      // alerts user if input is not a numeric value
     } else if (isNaN(userInput)) {
     alert("Please input only numbers.");
-    return writePassword();
+    return pwLength();
 
     } else {
-    console.log(userInput);
+    console.log("dude");
     }
+}
 
-//declaring character variables
+// how can user decide to exit out if they don't want to keep going?
+// passGroup needs to be only pwLength #s long, how to do that? passGroup.length === pwLength
+// passGroup.push needs to push a random number of only the specified groups of chars into the group
+
+function charSelect() {
+  var passGroup =[];
+  var lowerQ = confirm("Would you like lower case characters?");
+    if (lowerQ === true) {
+      passGroup.push(lowChar)
+    }
+  var upperQ = confirm("Would you like uppercase characters?");
+    if (upperQ === true) {
+      passGroup.push(upChar)
+   }
+  var numQ = confirm("Would you like numbers?");
+    if (numQ === true) {
+      passGroup.push(num)
+    }
+  var specQ = confirm("Would you like to use any special characters?");
+    if (specQ === true) {
+      passGroup.push(specChar)
+    }
+    
+    if (lowerQ === false &&
+        upperQ ===false &&
+        numQ === false &&
+        specQ === false) {
+      alert("You must select at least one of the character types.");
+      return charSelect();}   
+// make these each separate variable arrays
   var lowChar = [
   "a",
   "b",
@@ -121,53 +145,47 @@ function writePassword() {
   "}",
   "~",
   ];
-  
-  var passGroup = [];
-  var lowerQ = confirm("Would you like lower case characters?");
-    if (lowerQ === true) {
-      passGroup.push(lowChar);
-    }
-  var upperQ = confirm("Would you like uppercase characters?");
-    if (upperQ === true) {
-      passGroup.push(upChar);
-   }
-  var numQ = confirm("Would you like numbers?");
-    if (numQ === true) {
-      passGroup.push(num);
-    }
-  var specQ = confirm("Would you like to use any special characters?");
-    if (specQ === true) {
-      passGroup.push(specChar);
-    }
-    
-    if (lowerQ === false &&
-        upperQ ===false &&
-        numQ === false &&
-        specQ === false) {
-      alert("You must select at least one of the character types.");
-      return writePassword();}   
-// make these each separate variable arrays
- 
-  console.log(lowerQ, upperQ, numQ, specQ);
 
-  var result =""
- 
-  // for loop that goes through the character arrays selected to be used by the user 
-  for (var i = 0; i < userInput; i++) {
-    // store in bacon, take a random char from passGroup multiplied by length of passGroup
-      var bacon = passGroup[Math.floor(Math.random() * passGroup.length)];
-      result = result += bacon[i]
-  }
-  console.log(result);
-
-  return result;
-
-  // function generatePassword();
+  console.log(passGroup);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
 
+
+// var result =""
+
+// for (var i = 0; i < userInput; i++) {
+//     var password = passGroup[Math.floor(Math.random() * passGroup.length)];
+//     result = result += password[i]
+// }
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// placeholder
+function generatePassword() {
+  return ("¯\\_(ツ)_/¯");
+}
+
+// function to run through the character arrays in a for loop
+// function passChars(arr) {
+//   for (var i = 0; i > pwLength; i++);
+//   console.log(passGroup);
+// }
+
+// This is calling the function writePassword
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+pwLength(); 
+charSelect();
 
 // !--Begin pseudocode ==================================================================
 // User requests a password to be generated(clicks Generate Password button)
--- Assets/thinking.js
+++ Assets/thinking.js
@@ -2,3 +2,7 @@ function writePassword ()
     function generatePassword ()
         function pwLength ()
         function passChars ()
+
+var generatePassword = use pwLengthcharSelect
+
+ // return ("¯\\_(ツ)_/¯");
